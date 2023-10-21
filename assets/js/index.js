77a66795563f67e629459d9fe0d0b286984895e1
Vanessa Espitia <vanessa.espitia0408@gmail.com>
2023-10-21 12:06:06 -0600

    Avance a

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,17 @@
 console.log("Entro index.js");
+
+let contenidoProductos = document.getElementById("contenidoProductos");
+
+
+fetch('https://dev4humans.com.mx/api/clases/productos', {method: "GET"})
+    .then(response => response.json())
+    .then(data => {
+        console.log("La promesa se reolvio");
+        const rawData = data;
+        const productos = rawData.data.products;
+        console.log(productos);
+        productos.forEach(element => {
+            // contenidoProductos.innerHTML += '<p>demo</p>';
+            contenidoProductos.innerHTML = contenidoProductos.innerHTML + '<p>${producto.title}</p>';
+        });
+    }).catch(error => console.log(error))
